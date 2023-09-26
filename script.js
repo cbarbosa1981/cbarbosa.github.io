33d0a8342397f7c917a1b90ae68932a8d99e1023
cbarbosa1981 <106968899+cbarbosa1981@users.noreply.github.com>
2023-09-26 19:57:34 -0300

    Update script.js
    alteração JS para torna-lo responsivo

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Função para calcular o custo
 function calculate() {
     var kwhMonth = parseFloat(document.getElementById('kwh-month').value);
     var kwhCost = parseFloat(document.getElementById('kwh-cost').value);
@@ -12,3 +13,17 @@ function calculate() {
 
     document.getElementById('result').innerHTML = 'O ar condicionado gasta R$' + totalCost.toFixed(2) + ' por mês.';
 }
+
+// Adicionar tratamento de evento para o botão "Calcular"
+var calculateButton = document.querySelector('.btn');
+calculateButton.addEventListener('click', calculate);
+
+// Adicionar tratamento de evento para a tecla "Enter" nos campos de entrada
+var inputFields = document.querySelectorAll('.input-group input');
+inputFields.forEach(function (input) {
+    input.addEventListener('keyup', function (event) {
+        if (event.key === 'Enter') {
+            calculate();
+        }
+    });
+});
